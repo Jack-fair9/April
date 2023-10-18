c1bd61971035473e45bdb3606d91da900fcb0fa3
Jack-fair9 <Jack.fairfield@edu.sait.ca>
2023-10-17 19:13:26 -0600

    cel to far

---
+++ temp.py
@@ -0,0 +1,25 @@
+# Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32.
+def calc_temp(Ftemp):
+    celsuius * 9/5 + 32
+    return Ftemp
+    
+celsuius = 25
+
+#Ftemp = calc_temp
+Ftemp = celsuius * 9/5 + 32
+print(f'25 degrees celsius to fahrenheit is {Ftemp}')
+
+
+
+
+
+
+
+
+
+
+
+
+#def calc_temp(ToFahrenheit):
+    #celsuius * 9/5 + 32
+    
